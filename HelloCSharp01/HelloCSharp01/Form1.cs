001250716748dceed2a59873ae52dd1b083cccfe
letter333 <68866597+letter333@users.noreply.github.com>
2022-07-20 16:18:26 +0900

    Add files via upload

---
+++ HelloCSharp01/HelloCSharp01/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharp01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string pw = textBox2.Text;
+            MessageBox.Show("ID:"+id+", PW:"+pw);
+        }
+    }
+}
