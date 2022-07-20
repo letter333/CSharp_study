dcf336b3af34c288b3db71cd0d27993bda1f4ca7
letter333 <68866597+letter333@users.noreply.github.com>
2022-07-20 16:21:55 +0900

    Add files via upload

---
+++ CustomTabs/CustomTabs/Form1.cs
@@ -0,0 +1,53 @@
+﻿using CustomTabs.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomTabs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UC_Home uc = new UC_Home();
+            addUserControl(uc);
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            UC_Sent uc = new UC_Sent();
+            addUserControl(uc);
+        }
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            UC_Home uc = new UC_Home();
+            addUserControl(uc);
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            UC_Inbox uc = new UC_Inbox();
+            addUserControl(uc);
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            UC_Settings uc = new UC_Settings();
+            addUserControl(uc);
+        }
+    }
+}
