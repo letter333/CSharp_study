5f57b33cdfddd3ce3bdf21901b56535e76516e62
letter333 <seryong12@gmail.com>
2022-11-25 16:41:25 +0900

    C# Study with DJT 20221125

---
+++ HelloMyCSharp10/HelloMyCSarp10_04/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HelloMyCSarp10_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=21340000000";
+            XElement x = XElement.Load(url);
+            // Descendants = 자손들
+            var xq = from item in x.Descendants("data") select item;
+            foreach(var item in xq)
+            {
+                Console.WriteLine(item.Element("hour").Value);
+                Console.WriteLine(item.Element("wfKor").Value);
+                Console.WriteLine(item.Element("wfEn").Value);
+                Console.WriteLine(item.Element("temp").Value);
+            }
+            Console.WriteLine(x);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=21340000000";
+            XElement x = XElement.Load(url);
+            // Descendants = 자손들
+            var xq = from item in x.Descendants("data") select new
+            {
+                hour = item.Element("hour").Value,
+                wf = item.Element("wfKor").Value,
+                temp = item.Element("temp").Value
+            };
+            foreach (var item in xq)
+            {
+                Console.WriteLine(item.hour);
+                Console.WriteLine(item.wf);
+                Console.WriteLine(item.temp);
+            }
+            Console.WriteLine(x);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=21340000000";
+            XElement x = XElement.Load(url);
+            // Descendants = 자손들
+            var xq = from item in x.Descendants("data")
+                     select new Weather()
+                     {
+                         hour = int.Parse(item.Element("hour").Value),
+                         wf = item.Element("wfKor").Value.ToString(),
+                         temp = double.Parse(item.Element("temp").Value)
+                     };
+
+            List<Weather> w = xq.ToList<Weather>();
+            foreach (var item in xq)
+            {
+                Console.WriteLine(item.hour);
+                Console.WriteLine(item.wf);
+                Console.WriteLine(item.temp);
+            }
+            Console.WriteLine(x);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=21340000000";
+            XElement x = XElement.Load(url);
+            // Descendants = 자손들
+            var xq = from item in x.Descendants("data")
+                     select new Weather()
+                     {
+                         hour = int.Parse(item.Element("hour").Value),
+                         wf = item.Element("wfKor").Value.ToString(),
+                         temp = double.Parse(item.Element("temp").Value)
+                     };
+
+            List<Weather> w = xq.ToList<Weather>();
+
+            weatherBindingSource.DataSource = null;
+            weatherBindingSource.DataSource = w;
+            
+        }
+    }
+}
---
+++ HelloMyCSharp10/HelloMyCSharp10_02/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp10_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void printA()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Console.Write('A');
+            }
+        }
+
+        void printB()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write('B');
+            }
+        }
+
+        void printC()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write('C');
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printA();
+            printB();
+            printC();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(printA);
+            a.Start();
+            new Thread(printB).Start();
+            new Thread(printC).Start();
+            new Thread(delegate ()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write('D');
+                }
+            }).Start();
+            new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write('E');
+                }
+            });
+        }
+    }
+}
